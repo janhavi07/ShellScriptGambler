dff08304d61f4c9875b8c0ce744f4f5ad2dc6699
janhavi07 <partejanhavi@gmail.com>
2019-11-14 11:32:52 +0530

    bet so either win or loose

--- GamblerStimulator.sh
+++ GamblerStimulator.sh
@@ -6,3 +6,14 @@ echo WELCOME TO GAMBLER STIMULATOR
 stake=100
 bet=1
 
+winlost=$(( RANDOM%2 ))
+echo "HE BETS 1$"
+if [ $winlost -eq 1 ]
+then
+	echo WIN
+	stake=$(( $stake + $bet ))
+else
+	echo LOST
+	stake=$(( $stake - $bet ))
+fi
+echo "STAKE IS :" $stake
