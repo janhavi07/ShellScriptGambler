c73afaef56514ccf2667557a1f629a37844dcbef
janhavi07 <partejanhavi@gmail.com>
2019-11-14 12:40:52 +0530

    if won or lost 50% of the stake, would resign for the day

--- GamblerStimulator.sh
+++ GamblerStimulator.sh
@@ -4,16 +4,27 @@ echo WELCOME TO GAMBLER STIMULATOR
 
 #VARIABLES
 stake=100
-bet=1
+BET=1
+RESIGNVALUE=50
 
-winlost=$(( RANDOM%2 ))
-echo "HE BETS 1$"
-if [ $winlost -eq 1 ]
-then
-	echo WIN
-	stake=$(( $stake + $bet ))
-else
-	echo LOST
-	stake=$(( $stake - $bet ))
-fi
-echo "STAKE IS :" $stake
+winningStake=$(( ($RESIGNVALUE*$stake)/100  + $stake ))
+losingStake=$(( $stake - ($RESIGNVALUE*$stake)/100 ))
+
+while [ $stake -lt $winningStake  ] && [ $stake -gt $losingStake ]
+do
+	winlost=$(( RANDOM%2 ))
+	echo "HE BETS 1$"
+	if [ $winlost -eq 1 ]
+	then
+		echo WIN
+		stake=$(( $stake + $BET ))
+	else
+		echo LOST
+		stake=$(( $stake - $BET ))
+	fi
+	winningStake=$(( (50*$stake)/100 +$stake ))
+        losingStake=$(( $stake - (50*$stake)/100 ))
+
+done
+echo "RESIGNS FOR THE DAY"
+
